e711ce31758853c2ba09fe69d59d3b24660c667d
Андрей Левченков <lev_a_a@mail.ru>
2024-03-19 14:15:47 +0200

    Work 5-1

--- HomeWork-5-1/Program.cs
+++ HomeWork-5-1/Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-const double MM = 3.14;
+Console.Write("Введите значение в мм: ");
+double mm = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine($"Значение в сантиметрах: {mm/10} см");
+Console.WriteLine($"Значение в метрах: {mm / 1000} м");
+Console.WriteLine($"Значение в километрах: {mm / 1000000} км");
+Console.ReadLine();
