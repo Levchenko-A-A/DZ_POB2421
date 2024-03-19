15f3f20a2ffcbaef01caf3b8e6841e38aaced2a4
Андрей Левченков <lev_a_a@mail.ru>
2024-03-19 15:33:17 +0200

    Work5-1

--- HomeWork-5-1/Program.cs
+++ HomeWork-5-1/Program.cs
@@ -1,9 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+const double Ksm = 0.1;             // Коэффициент пересчета в самтиметры
+const double Km = 0.001;            // Коэффициент пересчета в метры
+const double Kkm = 0.000001;        // Коэффициент пересчета в километры
+const double Kmile = 0.000000621;   // Коэффициент пересчета в мили
+const double Kyard = 0.00109;       // Коэффициент пересчета в ярды
+
 Console.Write("Введите значение в мм: ");
-double mm = double.Parse(Console.ReadLine());
-
-
-Console.WriteLine($"Значение в сантиметрах: {mm/10} см");
-Console.WriteLine($"Значение в метрах: {mm / 1000} м");
-Console.WriteLine($"Значение в километрах: {mm / 1000000} км");
+double mm = double.Parse(Console.ReadLine()!);
+Console.WriteLine($"Значение в сантиметрах: {mm * Ksm} см\n" +
+    $"Значение в метрах: {mm * Km} м\n" +
+    $"Значение в километрах: {mm * Kkm} км\n" +
+    $"Значение в милях: {mm * Kmile} ми\n" +
+    $"Значение в ярдах: {mm * Kyard} ");
 Console.ReadLine();
