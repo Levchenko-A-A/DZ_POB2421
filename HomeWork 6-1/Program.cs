15f91d9411525a2a4bb764a12cb70ffdc29011cd
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-03-21 16:41:30 +0200

    HomeWork 6-1

---
+++ HomeWork 6-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+Console.Write("Введите число:");
+string textVar = Console.ReadLine();
+byte textVar_byte = Convert.ToByte(textVar);
+Console.WriteLine(textVar_byte);
+nint textVar_nint = Convert.ToInt32(textVar_byte);
+Console.WriteLine(textVar_nint);
+int textVar_int = Convert.ToInt(textVar_nint);
+
+
+Console.ReadLine();
