554a98260bba26c5e9b3bee365a7fe86e87cb439
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-03-21 20:35:31 +0200

    HomeWork 6-2

---
+++ HomeWork 6-2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.Write("Введите Фамилию: ");
+string surName = Console.ReadLine()!;
+Console.Write("Введите Имя: ");
+string nAme = Console.ReadLine()!;
+Console.Write("Введите Ваш возраст: ");
+int aGe = int.Parse(Console.ReadLine()!);
+Console.Write("Введите Ваш рост: ");
+int heIght = int.Parse(Console.ReadLine()!);
+
+
+string newString = String.Join(" ", surName, nAme, aGe.ToString(), heIght.ToString());
+Console.WriteLine(newString);
+
+Console.WriteLine($"\nТип данных переменной surName: {surName.GetType()}\n" +
+    $"Тип данных переменной nAme: {nAme.GetType()}\n" +
+    $"Тип данных переменной aGe: {aGe.GetType()}\n" +
+    $"Тип данных переменной heIght: {heIght.GetType()}\n");
