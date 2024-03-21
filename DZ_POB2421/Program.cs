d9d5a7660219a0ee9cc459890af073e14061de83
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-03-21 19:53:02 +0200

    HomeWork 6-1

--- DZ_POB2421/Program.cs
+++ DZ_POB2421/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine($"nint.MinValue = {nint.MinValue}");
+//Console.WriteLine($"nint.MaxValue = {nint.MinValue}");
+//Console.WriteLine($"int16.MinValue = {Int16.MinValue}");
+//Console.WriteLine($"int16.MaxValue = {Int16.MaxValue}");
+//Console.WriteLine($"int.MinValue = {Int32.MinValue}");
+//Console.WriteLine($"int.MaxValue = {Int32.MaxValue}");
+//Console.WriteLine($"int.MinValue = {Int64.MinValue}");
+//Console.WriteLine($"int.MaxValue = {Int64.MaxValue}");
+//Console.WriteLine($"int.MinValue = {int.MinValue}");
+//Console.WriteLine($"int.MaxValue = {int.MaxValue}");
--- HomeWork 6-1/Program.cs
+++ HomeWork 6-1/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System.Linq.Expressions;
 
 Console.Write("Введите число:");
-string textVar = Console.ReadLine();
-byte textVar_byte = Convert.ToByte(textVar);
+string textVar = Console.ReadLine();                        //Ввод целочисленной переменной
+byte textVar_byte = Convert.ToByte(textVar);                //Конвертация строковой переменной в переменную типа byte
 Console.WriteLine(textVar_byte);
-nint textVar_nint = Convert.ToInt32(textVar_byte);
+nint textVar_nint = Convert.ToInt32(textVar_byte);          //Конвертация типа byte в тип nint
 Console.WriteLine(textVar_nint);
-int textVar_int = Convert.ToInt(textVar_nint);
+int textVar_int = Convert.ToInt32(textVar_nint);            //Конвертация типа nint в тип int
+Console.WriteLine(textVar_int);
+long textVar_long = Convert.ToInt64(textVar_int);           //Конвертация типа int в тип long
+Console.WriteLine(textVar_long);
+double textVar_Double = Convert.ToDouble(textVar_long);     //Конвертация типа long в тип double
+Console.WriteLine(textVar_Double);
 
-
-Console.ReadLine();
+//Console.ReadLine();
