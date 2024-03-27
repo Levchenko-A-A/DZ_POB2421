dc4378862b758d59222569d109dc23550b65542b
Андрей Левченков <lev_a_a@mail.ru>
2024-03-27 22:51:02 +0200

    HomeWork8-1

--- HomeWork 8-1/Program.cs
+++ HomeWork 8-1/Program.cs
@@ -4,6 +4,7 @@
 //Сколько книг взял каждый мальчик?
 //Сколько книг они взяли вместе?
 
+Console.WriteLine("\nЗадача номер 1\n");
 int books = 0;
 int summ = 0;
 books += 10;
@@ -16,3 +17,11 @@ books += 2;
 summ += books;
 Console.WriteLine($"Алеша взял {books} книг.");
 Console.WriteLine($"Вместе ребята взяли {summ} книг.");
+
+//В первой корзине 23 яблока, а во второй на 11 яблок меньше.
+//Сколько яблок в обеих корзинах?
+
+Console.WriteLine("\nЗадача номер 2\n");
+int firstBasket = 23;
+int secondBasket = firstBasket - 11;
+Console.WriteLine($"В двух корзинах {firstBasket+secondBasket} яблок.");
