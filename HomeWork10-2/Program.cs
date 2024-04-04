1643a313de9a3bda819b4921ed72276321eb9fb4
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-04-04 11:08:16 +0200

    HomeWork10-2

---
+++ HomeWork10-2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Написать программу, которая запрашивает у пользователя значение
+//катета и гипотенузы прямоугольного треугольника, а затем выводит на
+//экран значение второго катета, вычисленное с помощью методов класса
+//Math.
+
+Console.Write("Введите значение 1-ого катета: ");
+double firstLeg = double.Parse(Console.ReadLine()!);
+Console.Write("Введите значение гипотенузы:");
+double hypotenuse = double.Parse(Console.ReadLine()!);
+Console.WriteLine($"Значение 2-ого катета: {Math.Round(Math.Sqrt(Math.Pow(hypotenuse,2)-Math.Pow(firstLeg,2)),2)}");
