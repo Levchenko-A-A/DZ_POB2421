64e4fb3229276793f83aec62c2903ec6feb2468f
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-04-04 10:57:13 +0200

    HomeWork10-1

--- HomeWork10-1/Program.cs
+++ HomeWork10-1/Program.cs
@@ -2,3 +2,7 @@
 //радиуса окружности и выводит на экран ее длину и площадь,
 //вычисленные с помощью методов класса Math.
 
+Console.WriteLine("Введите длину окружности: ");
+double R = double.Parse(Console.ReadLine());
+Console.WriteLine($"Длина окружности L = {Math.Round(2*Math.PI*R,2)}");
+Console.WriteLine($"Площадь окружности S = {Math.Round(Math.PI*Math.Pow(R,2),2)}");
