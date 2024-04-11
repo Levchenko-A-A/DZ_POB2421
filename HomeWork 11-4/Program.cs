d5c7e125a664145186654b1f8ddec478aaa3f7bd
Андрей Левченков <lev_a_a@mail.ru>
2024-04-11 21:37:07 +0200

    HomeWork 11-4

--- HomeWork 11-4/Program.cs
+++ HomeWork 11-4/Program.cs
@@ -1,4 +1,5 @@
 ﻿//Незнайка
+
 Console.Write("Введите Ваш возраст: ");
 int age = int.Parse(Console.ReadLine()!);
 Console.WriteLine($"Кандидаты в президенты солнечного города:\n" +
@@ -20,96 +21,33 @@ switch (choice)
     case 4: Console.WriteLine("Сейчас как треснемся об Луну, и привет Ромашке."); break;
 }
 
-//Задача на 10-ку
+//Задача на 10-ку 1
 
 Random random = new Random();
 int month = random.Next(1, 13);
 Console.Write("Введите количество дней в загаданном месяце: ");
 int daysInMonthUser = int.Parse(Console.ReadLine());
-switch (month)
+if (daysInMonthUser == System.DateTime.DaysInMonth(2024, month)) Console.WriteLine("Победа!!!");
+else Console.WriteLine($"Компьютер загадал {System.DateTime.DaysInMonth(2024, month)} количество дней.");
+
+
+
+//Задача на 10-ку 2
+
+Random random2 = new Random();
+int month2 = random2.Next(1, 13);
+switch (month2)
 {
-    case 1:
-        {
-            int daysInMonth = System.DateTime.DaysInMonth(2024, month);
-            Console.WriteLine($"Компьютер загадал: Январь");
-            Console.WriteLine((daysInMonthUser == daysInMonth) ? "Вы угадали" : "Вы не угадали"); 
-            break;
-        }
-    case 2:
-        {
-            int daysInMonth = System.DateTime.DaysInMonth(2024, month);
-            Console.WriteLine($"Компьютер загадал: Февраль");
-            Console.WriteLine((daysInMonthUser == daysInMonth) ? "Вы угадали" : "Вы не угадали");
-            break;
-        }
-    case 3:
-        {
-            int daysInMonth = System.DateTime.DaysInMonth(2024, month);
-            Console.WriteLine($"Компьютер загадал: Март");
-            Console.WriteLine((daysInMonthUser == daysInMonth) ? "Вы угадали" : "Вы не угадали");
-            break;
-        }
-    case 4:
-        {
-            int daysInMonth = System.DateTime.DaysInMonth(2024, month);
-            Console.WriteLine($"Компьютер загадал: Апрель");
-            Console.WriteLine((daysInMonthUser == daysInMonth) ? "Вы угадали" : "Вы не угадали");
-            break;
-        }
-    case 5:
-        {
-            int daysInMonth = System.DateTime.DaysInMonth(2024, month);
-            Console.WriteLine($"Компьютер загадал: Май");
-            Console.WriteLine((daysInMonthUser == daysInMonth) ? "Вы угадали" : "Вы не угадали");
-            break;
-        }
-    case 6:
-        {
-            int daysInMonth = System.DateTime.DaysInMonth(2024, month);
-            Console.WriteLine($"Компьютер загадал: Июнь");
-            Console.WriteLine((daysInMonthUser == daysInMonth) ? "Вы угадали" : "Вы не угадали");
-            break;
-        }
-    case 7:
-        {
-            int daysInMonth = System.DateTime.DaysInMonth(2024, month);
-            Console.WriteLine($"Компьютер загадал: Июль");
-            Console.WriteLine((daysInMonthUser == daysInMonth) ? "Вы угадали" : "Вы не угадали");
-            break;
-        }
-    case 8:
-        {
-            int daysInMonth = System.DateTime.DaysInMonth(2024, month);
-            Console.WriteLine($"Компьютер загадал: Август");
-            Console.WriteLine((daysInMonthUser == daysInMonth) ? "Вы угадали" : "Вы не угадали");
-            break;
-        }
-    case 9:
-        {
-            int daysInMonth = System.DateTime.DaysInMonth(2024, month);
-            Console.WriteLine($"Компьютер загадал: Сентябрь");
-            Console.WriteLine((daysInMonthUser == daysInMonth) ? "Вы угадали" : "Вы не угадали");
-            break;
-        }
-    case 10:
-        {
-            int daysInMonth = System.DateTime.DaysInMonth(2024, month);
-            Console.WriteLine($"Компьютер загадал: Октябрь");
-            Console.WriteLine((daysInMonthUser == daysInMonth) ? "Вы угадали" : "Вы не угадали");
-            break;
-        }
-    case 11:
-        {
-            int daysInMonth = System.DateTime.DaysInMonth(2024, month);
-            Console.WriteLine($"Компьютер загадал: Ноябрь");
-            Console.WriteLine((daysInMonthUser == daysInMonth) ? "Вы угадали" : "Вы не угадали");
-            break;
-        }
-    case 12:
-        {
-            int daysInMonth = System.DateTime.DaysInMonth(2024, month);
-            Console.WriteLine($"Компьютер загадал: Декабрь");
-            Console.WriteLine((daysInMonthUser == daysInMonth) ? "Вы угадали" : "Вы не угадали");
-            break;
-        }
+    case 1: Console.WriteLine($"Компьютер загадал: Январь"); break;
+    case 2: Console.WriteLine($"Компьютер загадал: Февоаль"); break;
+    case 3: Console.WriteLine($"Компьютер загадал: Март"); break;
+    case 4: Console.WriteLine($"Компьютер загадал: Апрель"); break;
+    case 5: Console.WriteLine($"Компьютер загадал: Май"); break;
+    case 6: Console.WriteLine($"Компьютер загадал: Июнь"); break;
+    case 7: Console.WriteLine($"Компьютер загадал: Июль"); break;
+    case 8: Console.WriteLine($"Компьютер загадал: Август"); break;
+    case 9: Console.WriteLine($"Компьютер загадал: Сентябрь"); break;
+    case 10: Console.WriteLine($"Компьютер загадал: Октябрь"); break;
+    case 11: Console.WriteLine($"Компьютер загадал: Ноябрь"); break;
+    case 12: Console.WriteLine($"Компьютер загадал: Декабрь"); break;
 }
