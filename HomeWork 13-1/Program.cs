d03a103dd107efa13dbabdc0e3c6f591a8feccee
Андрей Левченков <lev_a_a@mail.ru>
2024-04-15 22:30:19 +0200

    HomeWork 13-1

---
+++ HomeWork 13-1/Program.cs
@@ -0,0 +1,16 @@
+﻿Random random = new Random();
+int number1 = random.Next(1,6);
+int i = 1;
+Console.WriteLine("Компьютер загадал число от 1 до 5.\nУ Вас 3 попытки угадать.");
+while (i<=3)
+{
+    Console.Write($"Введите число. Попытка N{i++}: ");
+    int number2=int.Parse( Console.ReadLine()!);
+    if (number2 == number1)
+    {
+        Console.WriteLine("Вы угали!!!");
+        break;
+    }
+    else Console.WriteLine("Вы не угадали.");
+}
+Console.WriteLine($"Компьютер загадал {number1}");
