1cb5b6575b28d599ce56b770a1c858884359747b
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-04-18 09:43:52 +0200

    HomeWork 12-1

---
+++ HomeWork 11-3/Program.cs
@@ -0,0 +1,9 @@
+﻿Random random = new Random();
+
+int byte1=random.Next(100, 1000);
+
+Console.WriteLine(byte1);
+
+
+//Random random = new Random();
+//int n=random.Next(120);
---
+++ HomeWork 12-1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите число 1: ");
+double numberOne = double.Parse(Console.ReadLine()!);
+Console.Write("Введите число 2: ");
+double numberTwo = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Выберите математическую операцию:\n" +
+                  "1 - Cложение \"+\"\n" +
+                  "2 - Вычитание \"-\"\n" +
+                  "3 - Умножение \"*\"\n" +
+                  "4 - Деление \"\\\"");
+int operation=int.Parse(Console.ReadLine()!);
+switch (operation)
+{
+    case 1: Console.WriteLine($"Результат сложения: {numberOne + numberTwo}"); break;
+    case 2: Console.WriteLine($"Результат сложения: {numberOne - numberTwo}"); break;
+    case 3: Console.WriteLine($"Результат сложения: {numberOne * numberTwo}"); break;
+    case 4:
+        {
+            if (numberTwo == 0)
+            {
+                Console.WriteLine("Введен не корректно делитель, делить на 0 нельзя");
+                break;
+            }
+            else
+            {
+                Console.WriteLine($"Результат сложения: {numberOne / numberTwo}");
+                break;
+            }
+        }
+}
