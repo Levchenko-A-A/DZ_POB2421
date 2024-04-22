3aba03a54e95649c69f62f0710be117c9b42a9ae
Андрей Левченков <lev_a_a@mail.ru>
2024-04-22 23:15:45 +0200

    HomeWork 14-1

---
+++ HomeWork 14-1/Program.cs
@@ -0,0 +1,15 @@
+﻿Random random = new Random();
+char[] symbol = { '1','2','3','4','5','6','7','8','9','0','q','Q',
+                  'w','W','e','E','r','R','t','T','y','Y','u','U',
+                  'i','I','o','O','p','P','a','A','s','S','d','D',
+                  'f','F','g','G','h','H','j','J','k','K','l','L',
+                  'z','Z','x','X','c','C','v','V','b','B','n','N','m','M'};
+Console.Write("Введите длину генерируемого пароля: ");
+int quantSymbol = int.Parse(Console.ReadLine()!);
+char[] newPassword = new char[quantSymbol];
+newPassword[0] = symbol[5];
+for (int i = 0; i < quantSymbol; i++)
+    newPassword[i] = symbol[random.Next(symbol.Length)];
+Console.WriteLine();
+foreach (var item in newPassword)
+    Console.Write(item);
