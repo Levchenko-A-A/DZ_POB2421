1c8d644e59601306b52190ab453fc7e37615facb
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-04-25 09:49:00 +0200

    HomeWork 15-2

---
+++ HomeWork 15-2/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] mas= new int[10];
+Random random = new Random();
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = random.Next(101);
+    Console.Write(mas[i]+" ");
+}
+int count = 0;
+for (int i = 0; i < mas.Length; i++)
+{
+    if (mas[i] % 2 == 0) count++;  
+}
+Console.WriteLine();
+Console.WriteLine($"Колличество четных чисел: {count}");
