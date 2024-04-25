d08775dd13bdb2bbc39f14bddba69c0f1524cb7f
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-04-25 09:55:18 +0200

    HomeWork 15-2

--- HomeWork 15-2/Program.cs
+++ HomeWork 15-2/Program.cs
@@ -1,14 +1,8 @@
-﻿int[] mas= new int[10];
+﻿int[] mas= new int[10];                                             //сохдание массива из 10 ячеек
 Random random = new Random();
-for (int i = 0; i < mas.Length; i++)
-{
-    mas[i] = random.Next(101);
-    Console.Write(mas[i]+" ");
-}
-int count = 0;
-for (int i = 0; i < mas.Length; i++)
-{
-    if (mas[i] % 2 == 0) count++;  
-}
+for (int i = 0; i < mas.Length; i++) mas[i] = random.Next(101);     //заполнение массива случайными числами
+for (int i = 0; i < mas.Length; i++) Console.Write(mas[i] + " ");   //вывод на экран массива отдельным циклом
+int count = 0;                                                      //счеткик четных чисел
+for (int i = 0; i < mas.Length; i++) if (mas[i] % 2 == 0) count++;  //проверка в цикле количества четных чисел в массиве
 Console.WriteLine();
-Console.WriteLine($"Колличество четных чисел: {count}");
+Console.WriteLine($"Колличество четных чисел: {count}");            //вывод на экран количество четных чисел
