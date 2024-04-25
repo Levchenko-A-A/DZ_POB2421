a1e49b7f99d8584eddb322865c279f0428c92e7a
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-04-25 10:00:07 +0200

    HomeWork 15-2

--- HomeWork 15-2/Program.cs
+++ HomeWork 15-2/Program.cs
@@ -1,8 +1,15 @@
-﻿int[] mas= new int[10];                                             //сохдание массива из 10 ячеек
+﻿int[] mas= new int[10];                                             //создание массива из 10 ячеек
 Random random = new Random();
 for (int i = 0; i < mas.Length; i++) mas[i] = random.Next(101);     //заполнение массива случайными числами
 for (int i = 0; i < mas.Length; i++) Console.Write(mas[i] + " ");   //вывод на экран массива отдельным циклом
 int count = 0;                                                      //счеткик четных чисел
-for (int i = 0; i < mas.Length; i++) if (mas[i] % 2 == 0) count++;  //проверка в цикле количества четных чисел в массиве
-Console.WriteLine();
+for (int i = 0; i < mas.Length; i++)
+{
+    if (mas[i] == 0) continue;
+    if (mas[i] % 2 == 0)                                            //проверка в цикле количества четных чисел в массиве
+        count++;  
+}
+    Console.WriteLine();
 Console.WriteLine($"Колличество четных чисел: {count}");            //вывод на экран количество четных чисел
+
+//Написав код программы без перехода к описанию, увидел, что не сделал проверку на 0, добавил
