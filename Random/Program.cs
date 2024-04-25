4ad0596a9e144d57016d7768eef9ae8b150b871c
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-04-25 11:57:37 +0200

    Random

---
+++ Random/Program.cs
@@ -0,0 +1,16 @@
+﻿Random rnd = new();
+string[] malePetNames = [ "Rufus", "Bear", "Dakota", "Fido",
+                        "Vanya", "Samuel", "Koani", "Volodya",
+                        "Prince", "Yiska" ];
+string[] femalePetNames = [ "Maggie", "Penny", "Saya", "Princess",
+                          "Abby", "Laila", "Sadie", "Olivia",
+                          "Starlight", "Talla" ];
+
+// Generate random indexes for pet names.
+int mIndex = rnd.Next(malePetNames.Length);
+int fIndex = rnd.Next(femalePetNames.Length);
+
+// Display the result.
+Console.WriteLine("Suggested pet name of the day: ");
+Console.WriteLine($"   For a male:     {malePetNames[mIndex]}");
+Console.WriteLine($"   For a female:   {femalePetNames[fIndex]}");
