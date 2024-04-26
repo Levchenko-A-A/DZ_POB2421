023954a671475b5ccee7031f7b536df35da6a6b0
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-04-26 12:02:55 +0200

    HomeWork 16-1

--- HomeWork 16-1/Program.cs
+++ HomeWork 16-1/Program.cs
@@ -1,36 +1,65 @@
-﻿double[,] array = new double[3, 3];
+﻿//1.Создать двумерный массив 3х3. Заполнить его дробными числами от
+//0 до 20.
+//Оформить вывод на экран всех чисел.
+//Добавить возможность замены значения пользователем.
+//Вывод нового массива.
+
+//double[,] array = new double[3, 3];
+//Random random = new Random();
+//for (int i = 0; i < array.GetLength(0); i++)
+//{
+//    for (int j = 0; j < array.GetLength(1); j++)
+//    {
+//        array[i, j] = random.NextDouble()*100/5;
+//    }
+//}
+//for (int i = 0; i < array.GetLength(0); i++)
+//{
+//    for (int j = 0; j < array.GetLength(1); j++)
+//    {
+//        Console.Write($"{(array[i,j]),5:F2} "); ;
+//    }
+//    Console.WriteLine();
+//}
+//Console.WriteLine();
+//Console.WriteLine("Для замены одного значения введите:");
+//int indexOne = 0;
+//int indexTwo = 0;
+//Console.Write("Номер строки: ");
+//indexOne=int.Parse(Console.ReadLine()!);
+//Console.Write("Номер столбца: ");
+//indexTwo = int.Parse(Console.ReadLine()!);
+//Console.WriteLine("Новое значение: ");
+//array[indexOne-1, indexTwo-1] = double.Parse(Console.ReadLine()!);
+//Console.WriteLine();
+//for (int i = 0; i < array.GetLength(0); i++)
+//{
+//    for (int j = 0; j < array.GetLength(1); j++)
+//    {
+//        Console.Write($"{(array[i, j]),5:F2} "); ;
+//    }
+//    Console.WriteLine();
+//}
+
+//2.Создать массив символов 5х2.
+//Заполнить его символами по желанию в разнобой.
+//Вывести на экран понятное слово.
+
+string symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char[,] array = new char[5, 2];
 Random random = new Random();
 for (int i = 0; i < array.GetLength(0); i++)
 {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-        array[i, j] = random.NextDouble()*100/5;
+        array[i, j] = symbols[random.Next(symbols.Length)];
     }
 }
 for (int i = 0; i < array.GetLength(0); i++)
 {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-        Console.Write($"{(array[i,j]),5:F2} "); ;
-    }
-    Console.WriteLine();
-}
-Console.WriteLine();
-Console.WriteLine("Для замены одного значения введите:");
-int indexOne = 0;
-int indexTwo = 0;
-Console.Write("Номер строки: ");
-indexOne=int.Parse(Console.ReadLine()!);
-Console.Write("Номер столбца: ");
-indexTwo = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Новое значение: ");
-array[indexOne-1, indexTwo-1] = double.Parse(Console.ReadLine()!);
-Console.WriteLine();
-for (int i = 0; i < array.GetLength(0); i++)
-{
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-        Console.Write($"{(array[i, j]),5:F2} "); ;
+        Console.Write(array[i, j] + " ");
     }
     Console.WriteLine();
 }
