39ba419a136f01424c1c590ae2ec841e5f27c9e2
Андрей Левченков <lev_a_a@mail.ru>
2024-05-22 22:39:46 +0200

    HomeWork20-2

---
+++ HomeWork20-2/Program.cs
@@ -0,0 +1,16 @@
+﻿Random random = new Random();
+int[] mas = new int[random.Next(5, 11)];
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = random.Next(10, 100);
+    Console.Write(mas[i] + " ");
+}
+Console.WriteLine($"Среднее арифметическое {summMas(mas):F2}");
+
+double summMas(int[] mas)
+{
+    double s = 0;
+    for (int i = 0; i < mas.Length; i++)
+        s += mas[i];
+    return s / mas.Length;
+}
