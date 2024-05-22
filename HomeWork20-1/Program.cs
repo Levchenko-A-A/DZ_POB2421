b2d78a5a053adae14e477d03ac72977b3310e0fb
Андрей Левченков <lev_a_a@mail.ru>
2024-05-22 22:34:30 +0200

    HomeWork20-1

---
+++ HomeWork20-1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Write("Введите первое число:");
+double fistNumber=double.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число:");
+double secondNumber = double.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма двух чисел: {Summ(fistNumber, secondNumber):F2}");
+
+double Summ(double a, double b)
+{
+    return a + b;
+}
