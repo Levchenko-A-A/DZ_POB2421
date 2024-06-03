c5ba305c87e94d6706995cf806134486ffa050df
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-06-03 08:47:17 +0200

    HomeWork 20-3

---
+++ HomeWork20-3/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задание 1.
+//Напишите функцию, которая принимает два числа и выводит на экран их
+//сумму.
+//Числа передаются пользователем в переменные, которые затем
+//передаются в функцию.
+//Функция должна возвращать результат, который в последствии выводится
+//на консоль.
+
+Console.Write("Введите первое число:");
+double fistNumber = double.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число:");
+double secondNumber = double.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма двух чисел: {Summ(fistNumber, secondNumber):F2}");
+
+double Summ(double a, double b)
+{
+    return a + b;
+}
+
+//Задание 2.
+//Напишите функцию, которая принимает массив целых чисел и возвращает
+//среднее значение элементов массива.
+//Массив задать явно
+//Функция должна возвращать результат, который в последствии выводится
+//на консоль.
+
+Random random = new Random();
+int[] mas = new int[random.Next(5, 11)];
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = random.Next(10, 100);
+    Console.Write(mas[i] + " ");
+}
+Console.WriteLine($"Среднее арифметическое {summMas(mas):F2}");
+
+double summMas(int[] mas)
+{
+    double s = 0;
+    for (int i = 0; i < mas.Length; i++)
+        s += mas[i];
+    return s / mas.Length;
+}
