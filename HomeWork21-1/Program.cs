e1b5e89d594d3bbe710cce9a3a803d6551872bd2
Андрей Левченков <lev_a_a@mail.ru>
2024-06-12 09:31:55 +0200

    HomeWork21-1

---
+++ HomeWork21-1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите путь к папке:");
+string path = Console.ReadLine()!;
+Console.Write("Введите название файла:");
+string fileName = Console.ReadLine()!;
+if (!File.Exists(path + fileName))
+    {
+        File.Create(path + fileName);
+        Console.WriteLine("Файл не был найден и создан");
+    }
+else Console.WriteLine("Файл найден");
+Console.Write("Введите текст:");
+string textFoFile = Console.ReadLine()!;
+File.WriteAllTextAsync(path + fileName, textFoFile);
+Console.WriteLine("Файл успешно записан.");
+Console.WriteLine("Содержимое аписанного файла:");
+Console.WriteLine(await File.ReadAllTextAsync(path + fileName));
