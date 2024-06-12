48d560bddbae737c39a084916c517892ab77c1da
Андрей Левченков <lev_a_a@mail.ru>
2024-06-12 09:44:52 +0200

    HomeWork21-2

---
+++ HomeWork21-2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите путь к папке:");
+string path = Console.ReadLine()!;
+Console.Write("Введите название файла:");
+string fileName = Console.ReadLine()!;
+if (!File.Exists(path + fileName))
+{
+    File.Create(path + fileName);
+    Console.WriteLine("Файл не был найден и создан");
+}
+else Console.WriteLine("Файл найден");
+FileInfo fileInfo = new FileInfo(path + fileName);
+Console.WriteLine($"Размер файла: {fileInfo.Length} байт");
+Console.WriteLine($"Дата последнего изменения: {fileInfo.LastWriteTime}");
+Console.WriteLine($"Расширение файла: {fileInfo.Extension}");
+
