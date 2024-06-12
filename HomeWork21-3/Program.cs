97e1c559af127d32cb450c03e3bf63b8c289d85e
Андрей Левченков <lev_a_a@mail.ru>
2024-06-12 11:14:03 +0200

    HomeWork21-3

---
+++ HomeWork21-3/Program.cs
@@ -0,0 +1,32 @@
+﻿string dirName = Directory.GetCurrentDirectory();
+Console.Clear();
+
+do
+{
+    DirectoryInfo dirInfo = new DirectoryInfo(dirName);
+    Console.Clear();
+    Console.WriteLine($"Текущая директория: {dirInfo.FullName}");
+    Console.WriteLine("Подкаталоги:");
+    DirectoryInfo[] dirs = dirInfo.GetDirectories();
+    foreach (DirectoryInfo dir in dirs)
+    {
+        Console.WriteLine(dir.Name);
+    }
+    Console.WriteLine();
+    Console.WriteLine("Файлы:");
+    FileInfo[] files = dirInfo.GetFiles();
+    foreach (FileInfo file in files)
+    {
+        Console.WriteLine(file.Name);
+    }
+    Console.WriteLine("Введите название папки для перехода (или 'выход' для завершения):");
+    string choice = Console.ReadLine()!;
+    if (choice == "выход") break;
+    else if (choice != "выход")
+    {
+        dirInfo.CreateSubdirectory(choice);
+        dirName += "\\";
+        dirName += choice;
+    }    
+}
+while (true);
