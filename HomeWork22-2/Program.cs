38f68b58b98548c55a006304d724757dc1ad40df
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-06-18 10:50:17 +0200

    HomeWork22-2

---
+++ HomeWork22-2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите список чисел через пробел:");
+string listNumber = Console.ReadLine()!;
+try
+{
+    string[] strings = listNumber.Split(' ');
+    foreach (string s in strings)
+        Console.Write(s + " ");
+    double result = 0;
+    for (int i = 0; i < strings.Length; i++)
+        result += Convert.ToDouble(strings[i]);
+    Console.WriteLine(result);
+}
+catch (FormatException)
+{
+    Console.WriteLine("Ошибка: некорректный ввод. Пожалуйста, введите только числа, разделенные пробелами.");
+}
