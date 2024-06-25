9684e31682db5fc09a6169cb9e0241a80294503a
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-06-25 16:52:51 +0200

    HomeWork24-1

---
+++ HomeWork24-1/Program.cs
@@ -0,0 +1,24 @@
+﻿string s;
+s = Console.ReadLine()!;
+if (Polindrom(s))
+    Console.WriteLine("Введенное слово является полиндромом");
+else
+    Console.WriteLine("Введенное слово не является полиндромом");
+
+
+bool Polindrom(String s)
+{
+    char firstSymbol;
+    char lastSymbol;
+    int size = s.Length;
+    String subString;
+    if (size <= 1)
+        return true;
+    else
+    {
+        firstSymbol = s[0];
+        lastSymbol = s[size - 1];
+        subString = s.Substring(1, size - 2);
+        return firstSymbol == lastSymbol && Polindrom(subString);
+    }
+}
