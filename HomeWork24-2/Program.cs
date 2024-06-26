79a7a1d136b1f1574681d3fa2c30c11db77ae96c
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-06-26 09:40:06 +0200

    HomeWork24-2

---
+++ HomeWork24-2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+Console.Write("Введите число:");
+int a;
+int.TryParse(Console.ReadLine(), out a);
+OutOddNumber(a - 1);
+
+void OutOddNumber(int n)
+{
+    try
+    {
+
+        if (n > 0)
+        {
+
+            if (n % 2 == 1)
+            {
+
+                OutOddNumber(n - 1);
+                Console.WriteLine(n);
+            }
+            else
+            {
+                OutOddNumber(n - 1);
+            }
+
+
+        }
+        else return;
+    }
+    catch (StackOverflowException e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
