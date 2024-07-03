741c3d7d6048228b17b5f6edbe6b8b93e5069e56
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-07-03 11:44:03 +0200

    HomeWork25-2

---
+++ HomeWork25-2/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace HomeWork25_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            linkLabel1 = new LinkLabel();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.Location = new Point(12, 342);
+            label1.Name = "label1";
+            label1.Size = new Size(275, 60);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            label2.Location = new Point(12, 428);
+            label2.Name = "label2";
+            label2.Size = new Size(268, 24);
+            label2.TabIndex = 1;
+            label2.Text = "with meditation";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            label3.Location = new Point(12, 402);
+            label3.Name = "label3";
+            label3.Size = new Size(268, 26);
+            label3.TabIndex = 2;
+            label3.Text = "Fell less stressed and more minful";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.Black;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            textBox1.ForeColor = SystemColors.InactiveBorder;
+            textBox1.Location = new Point(56, 517);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(196, 23);
+            textBox1.TabIndex = 3;
+            textBox1.Text = "  login                       →";
+            // 
+            // label4
+            // 
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            label4.Location = new Point(12, 573);
+            label4.Name = "label4";
+            label4.Size = new Size(268, 26);
+            label4.TabIndex = 4;
+            label4.Text = "Don't have an account?";
+            label4.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.ActiveLinkColor = Color.Black;
+            linkLabel1.BackColor = Color.Transparent;
+            linkLabel1.DisabledLinkColor = Color.Black;
+            linkLabel1.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            linkLabel1.ForeColor = Color.Black;
+            linkLabel1.LinkColor = Color.Black;
+            linkLabel1.Location = new Point(180, 574);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(95, 25);
+            linkLabel1.TabIndex = 5;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Sing up";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(292, 633);
+            Controls.Add(linkLabel1);
+            Controls.Add(label4);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            MaximumSize = new Size(310, 680);
+            MinimumSize = new Size(310, 680);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Окно приветствия";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label4;
+        private LinkLabel linkLabel1;
+    }
+}
