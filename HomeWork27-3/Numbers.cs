1f5921e55a393682f0d0753fa4f61ea774b5cbc1
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-07-12 11:12:58 +0200

    HomeWork27-3

---
+++ HomeWork27-3/Numbers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork27_3
+{
+    internal class Numbers
+    {
+        public void GeneretAverage(double[] mas)
+        {
+            Random random = new Random();
+            for (int i = 0; i < mas.Length; i++) 
+            {
+                mas[i]=random.NextDouble()*100;
+                Console.Write($"{mas[i]:F2} ");
+            }
+        }
+        public double CalculateAverage(double[] mas)
+        {
+            double result=0;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                result += mas[i];
+            }
+            return result/mas.Length;
+        }
+        public bool IsPrime (int number)
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        public bool IsEven (int number) 
+        {
+            if((number % 2) == 0) return true;
+            else return false;
+        }
+        public bool IsOdd(int number)
+        {
+            if ((number % 2) == 0) return false;
+            else return true;
+        }
+    }
+}
---
+++ HomeWork27-3/Program.cs
@@ -0,0 +1,26 @@
+﻿using HomeWork27_3;
+Numbers numbers = new Numbers();
+int num;
+Console.WriteLine("Метод - среднее арифметическое:");
+Random random = new Random();
+double[] mas=new double[random.Next(6,11)];
+numbers.GeneretAverage(mas);
+Console.WriteLine($"\nСреднее арифметическое: {numbers.CalculateAverage(mas):F2}\n");
+
+Console.WriteLine("Метод, определяющий простое число:");
+Console.Write("Введите число:");
+int.TryParse(Console.ReadLine()!, out num);
+if (numbers.IsPrime(num)) Console.WriteLine("Число простое.");
+else Console.WriteLine("Число не простое");
+
+Console.WriteLine("\nМетод, определяющий четность числи:");
+Console.Write("Введите число:");
+int.TryParse(Console.ReadLine()!, out num);
+if (numbers.IsEven(num)) Console.WriteLine("Число четное");
+else Console.WriteLine("Число нечетное");
+
+Console.WriteLine("\nМетод, определяющий четность числи:");
+Console.Write("Введите число:");
+int.TryParse(Console.ReadLine()!, out num);
+if (numbers.IsOdd(num)) Console.WriteLine("Число нечетное");
+else Console.WriteLine("Число четное");
