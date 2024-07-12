2f782d36b35d1a06e74ea9093f57db7f76722f00
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-07-12 11:54:18 +0200

    HomeWork27-4

---
+++ HomeWork27-4/Numbers.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork27_4
+{
+    internal class Numbers
+    {
+        public void GeneretAverage(double[] mas)
+        {
+            Random random = new Random();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = random.NextDouble() * 100;
+                Console.Write($"{mas[i]:F2} ");
+            }
+        }
+        public double CalculateAverage(double[] mas)
+        {
+            double result = 0;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                result += mas[i];
+            }
+            return result / mas.Length;
+        }
+        public bool IsPrime(int number)
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        public bool IsEven(int number)
+        {
+            if ((number % 2) == 0) return true;
+            else return false;
+        }
+        public bool IsOdd(int number)
+        {
+            if ((number % 2) == 0) return false;
+            else return true;
+        }
+        public int CalculateFactorial(int number)
+        {
+            return number == 0 ? 1 : CalculateFactorial(number - 1) * number;
+        }
+        public string ConvertToBinary(int number)
+        {
+            return Convert.ToString(number, 2);
+        }
+        public string ConvertToHex(int number)
+        {
+            return Convert.ToString(number, 16).ToUpper();
+        }
+        public double CalculatePower (int number, int power)
+        {
+            return Math.Pow(number, power);
+        }
+    }
+}
---
+++ HomeWork27-4/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using HomeWork27_4;
+using System;
+Numbers numbers = new Numbers();
+int num;
+int num2;
+Console.WriteLine("\nМетод вычисления факториала числа:");
+Console.Write("Введите число:");
+int.TryParse(Console.ReadLine()!, out num);
+Console.WriteLine($"Факториал Числа: {numbers.CalculateFactorial(num)}");
+
+Console.WriteLine("\nМетод преобразования в двоичное представление:");
+Console.Write("Введите число:");
+int.TryParse(Console.ReadLine()!, out num);
+Console.WriteLine($"Двоичное представление: {numbers.ConvertToBinary(num)}");
+
+Console.WriteLine("\nМетод преобразования в двоичное представление:");
+Console.Write("Введите число:");
+int.TryParse(Console.ReadLine()!, out num);
+Console.WriteLine($"Шестнадцатеричное представление: {numbers.ConvertToHex(num)}");
+
+Console.WriteLine("\nМетод возведения в степень:");
+Console.Write("Введите основание:");
+int.TryParse(Console.ReadLine()!, out num);
+Console.Write("Введите степень:");
+int.TryParse(Console.ReadLine()!, out num2);
+Console.WriteLine($"Результат возведения в степень: {numbers.CalculatePower(num, num2)}");
