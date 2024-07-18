21346a0aa27454649b4560c0068a3b94e1176318
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-07-18 17:03:22 +0200

    HomeWork20-3

--- HomeWork20-3/Program.cs
+++ HomeWork20-3/Program.cs
@@ -1,42 +1,104 @@
-﻿//Задание 1.
-//Напишите функцию, которая принимает два числа и выводит на экран их
-//сумму.
-//Числа передаются пользователем в переменные, которые затем
-//передаются в функцию.
-//Функция должна возвращать результат, который в последствии выводится
-//на консоль.
-
-Console.Write("Введите первое число:");
-double fistNumber = double.Parse(Console.ReadLine()!);
-Console.Write("Введите второе число:");
-double secondNumber = double.Parse(Console.ReadLine()!);
-Console.WriteLine($"Сумма двух чисел: {Summ(fistNumber, secondNumber):F2}");
-
-double Summ(double a, double b)
+﻿bool tmp = true;
+do
 {
-    return a + b;
+    Console.WriteLine("Меню:");
+    Console.WriteLine("1. Логическое умножение.");
+    Console.WriteLine("2. Логическое сложение.");
+    Console.WriteLine("3. Логическое исключение.");
+    Console.WriteLine("4. Выход.");
+    Console.Write("Ваш выбор:");
+    int choice;
+    int.TryParse(Console.ReadLine(), out choice);
+    switch (choice)
+    {
+        case 1:
+            {
+                Console.Write("Введите первое число:");
+                int namberOne;
+                int.TryParse(Console.ReadLine()!, out namberOne);
+                Console.Write("Введите первое число:");
+                int namberTwo;
+                int.TryParse(Console.ReadLine()!, out namberTwo);
+                BinMul(namberOne, namberTwo);
+                Console.ReadKey();
+                Console.Clear();
+            }
+            break;
+        case 2:
+            {
+                Console.Write("Введите первое число:");
+                int namberOne;
+                int.TryParse(Console.ReadLine()!, out namberOne);
+                Console.Write("Введите первое число:");
+                int namberTwo;
+                int.TryParse(Console.ReadLine()!, out namberTwo);
+                BinSum(namberOne, namberTwo);
+                Console.ReadKey();
+                Console.Clear();
+            }
+            break;
+        case 3:
+            {
+                Console.Write("Введите первое число:");
+                int namberOne;
+                int.TryParse(Console.ReadLine()!, out namberOne);
+                Console.Write("Введите ключ:");
+                int namberTwo;
+                int.TryParse(Console.ReadLine()!, out namberTwo);
+                BinExceptionKey(namberOne, namberTwo);
+                Console.ReadKey();
+                Console.Clear();
+            }
+            break;
+        case 4: tmp=false; break;
+    }
+} while (tmp);
+
+
+void BinSum(int value1,  int value2)
+{
+    value1 = 8;
+    value2 = 3;
+    string binary1 = Convert.ToString(value1, 2);
+    string binary2 = Convert.ToString(value2, 2);
+    Console.WriteLine($"DEC: {value1,6}\n" +
+                      $"DEC: {value2,6}\n" +
+                      $"BIN: {binary1,6}\n" +
+                      $"BIN: {binary2,6}\n" +
+                      $"Результат &:\n" +
+                      $"DEC: {value1 & value2,6}\n" +
+                      $"BIN: {Convert.ToString(value1 & value2, 2),6}\n");
 }
-
-//Задание 2.
-//Напишите функцию, которая принимает массив целых чисел и возвращает
-//среднее значение элементов массива.
-//Массив задать явно
-//Функция должна возвращать результат, который в последствии выводится
-//на консоль.
-
-Random random = new Random();
-int[] mas = new int[random.Next(5, 11)];
-for (int i = 0; i < mas.Length; i++)
+void BinMul(int value1, int value2)
 {
-    mas[i] = random.Next(10, 100);
-    Console.Write(mas[i] + " ");
+    value1 = 65;
+    value2 = 29;
+    string binary1 = Convert.ToString(value1, 2);
+    string binary2 = Convert.ToString(value2, 2);
+    Console.WriteLine($"DEC: {value1,7}\n" +
+                      $"DEC: {value2,7}\n" +
+                      $"BIN: {binary1,7}\n" +
+                      $"BIN: {binary2,7}\n" +
+                      $"Результат |:\n" +
+                      $"DEC: {value1 | value2,7}\n" +
+                      $"BIN: {Convert.ToString(value1 | value2, 2),7}\n");
 }
-Console.WriteLine($"Среднее арифметическое {summMas(mas):F2}");
-
-double summMas(int[] mas)
+void BinExceptionKey (int value1, int value2)
 {
-    double s = 0;
-    for (int i = 0; i < mas.Length; i++)
-        s += mas[i];
-    return s / mas.Length;
+    Console.WriteLine("Задача 3");
+    value1 = 249;
+    int key = 8;
+    value2 = value1 ^ key;
+    string binary1 = Convert.ToString(value1, 2);
+    string binary2 = Convert.ToString(key, 2);
+    Console.WriteLine($"DEC: {value1,8}\n" +
+                      $"DEC: {key,8}\n" +
+                      $"BIN: {binary1,8}\n" +
+                      $"BIN: {binary2,8}\n" +
+                      $"Результат 249^8:\n" +
+                      $"DEC: {value1 ^ key,8}\n" +
+                      $"BIN: {Convert.ToString(value1 ^ key, 2),8}\n" +
+                      $"Обратное исключение:\n" +
+                      $"DEC: {value2 ^ key,8}\n" +
+                      $"BIN: {Convert.ToString(value2 ^ key, 2),8}\n");
 }
