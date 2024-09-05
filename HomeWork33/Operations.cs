d9a2b8f97506caa61f16d8965d3634cf52b6812b
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-09-05 19:18:07 +0200

    HomeWork33

--- HomeWork33/Operations.cs
+++ HomeWork33/Operations.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace HomeWork33
@@ -16,6 +19,59 @@ namespace HomeWork33
             {
                 Console.Write("Введите " + (i + 1) + " строку:");
                 ListString.Add(Console.ReadLine()!);
+            }
+        }
+
+        public int FindIndex(string name)
+        {
+            if (ListString.BinarySearch(name) >= 0)
+                return ListString.BinarySearch(name);
+            else return -1;
+        }
+
+        public void FindList(string name)
+        {
+            Console.WriteLine(ListString.BinarySearch(name)+" "+ListString.Find(x => x == name));
+        }
+
+        public void RemoveAtIndex()
+        {
+            Console.Write("Введите индекс записи, которую необходимо удалить:");
+            int index;
+            int.TryParse(Console.ReadLine()!, out index);
+            ListString.RemoveAt(index-1);
+        }
+
+        public void Clear()
+        {
+            ListString.Clear();
+        }
+
+        public void SortList()
+        {
+            ListString.Sort();
+            //if (ListString.Count != 0)
+            //{
+            //    for (int i = 1; i < ListString.Count; i++)
+            //    {
+            //        List<string> key = ListString[i];
+            //        int j = i - 1;
+            //        while (j >= 0 && ListString[i].CompareTo(key))
+            //        {
+            //            ListString[j + 1] = ListString[j];
+            //            j--;
+            //        }
+            //        ListString[j + 1] = key;
+            //    }
+            //}
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{ "Номер записи",13}  {"Содержимое строки"}");
+            for (int i = 0; i < ListString.Count; i++)
+            {
+                Console.WriteLine($"{i+1,13}   {ListString[i]}");
             }
         }
     }
--- HomeWork33/Program.cs
+++ HomeWork33/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HomeWork33;
+Operations operations = new Operations();
+
+operations.Print();
+//operations.RemoveAtIndex();
+//operations.Print();
+
+Console.Write("Введите название запись для поиска:");
+string name = Console.ReadLine()!;
+if (operations.FindIndex(name)>=0)
+    Console.WriteLine(operations.FindIndex(name));
+else 
+    Console.WriteLine("Введеное значение не найдено!!!");
+
+operations.FindList(name);
+
+operations.Print();
+operations.SortList();
+operations.Print();
+operations.Clear();
+operations.Print();
