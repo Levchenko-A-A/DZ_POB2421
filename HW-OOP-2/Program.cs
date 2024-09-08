657ab163af0a8c504dc331750b8769e91c4d37d8
Андрей Левченков <lev_a_a@mail.ru>
2024-09-08 18:36:06 +0200

    oop2

--- HW-OOP-2/Program.cs
+++ HW-OOP-2/Program.cs
@@ -1,5 +1,5 @@
 ﻿using HW_OOP_2;
-
+// Задание 1
 //Console.WriteLine("Введите необходимое значение элементов массива");
 //int n;
 //int.TryParse(Console.ReadLine(), out n);
@@ -13,6 +13,8 @@
 //array.masPrint();
 //Console.WriteLine();
 //array.masCount();
+
+//Задание 2
 
 Library library = new Library();
 for (int i = 0; i < 5; i++)
@@ -28,17 +30,24 @@ for (int i = 0; i < 5; i++)
 Console.Clear();
 library.sortName();
 library.Print();
-Console.Write("ведите название книги:");
+Console.Write("Для удаления книги по названию, ведите название книги:");
 string nameBook = Console.ReadLine()!;
 library.SubName(nameBook);
 library.Print();
-//library.Print();
-//Console.Write("Для вывода списка по названию, ведите название книги:");
-//string nameBook = Console.ReadLine()!;
-//library.FindName(nameBook);
-//Console.Write("Для вывода списка по автору, ведите автора:");
-//string autorBook = Console.ReadLine()!;
-//library.FindAutor(autorBook);
-//Console.Write("Для вывода списка по году издания, введите год издания:");
-//int yearBook = int.Parse(Console.ReadLine()!);
-//library.FindYear(yearBook);
+Console.Write("Для удаления книги по автору, ведите автора:");
+string autorBook = Console.ReadLine()!;
+library.SubAutor(autorBook);
+library.Print();
+Console.Write("Для удаления книги по году издания, введите год издания:");
+int yearBook = int.Parse(Console.ReadLine()!);
+library.SubYear(yearBook);
+library.Print();
+Console.Write("Для вывода списка по названию, ведите название книги:");
+nameBook = Console.ReadLine()!;
+library.FindName(nameBook);
+Console.Write("Для вывода списка по автору, ведите автора:");
+autorBook = Console.ReadLine()!;
+library.FindAutor(autorBook);
+Console.Write("Для вывода списка по году издания, введите год издания:");
+yearBook = int.Parse(Console.ReadLine()!);
+library.FindYear(yearBook);
