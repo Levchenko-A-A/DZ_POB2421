975ccf2496b829c93f53797dc47ebafe23df79b2
Андрей Левченков <lev_a_a@mail.ru>
2024-09-08 15:07:34 +0200

    OOP1

---
+++ HW-OOP-1/Program.cs
@@ -0,0 +1,17 @@
+﻿using HW_OOP_1;
+
+Time time = new Time() { Hour = 20, Minute = 15, Second = 30 };
+time.Print();
+time.AddHour(10);
+time.Print();
+time.SubMinutes(50);
+time.SubHours(12);
+time.Print();
+
+double a = 3;
+double b = 4;
+double c = 5;
+Triangle triangle = new Triangle(a, b, c);
+triangle.Print();
+triangle.A = 8;
+triangle.Print();
