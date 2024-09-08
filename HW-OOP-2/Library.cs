a26e58ea2d4ecdb0b89b985e1659acb8df8c447e
Андрей Левченков <lev_a_a@mail.ru>
2024-09-08 16:39:22 +0200

    OOP2

---
+++ HW-OOP-2/Library.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_OOP_2
+{
+    struct Book
+    {
+        public string Name { get; set; }
+        public string Autor { get; set; }
+        public int Year { get; set; }
+    }
+
+    internal class Library
+    {
+        public List<Book> Books { get; set; } = new();
+        public void Add(Book book) => Books.Add(book);
+        public Library(List<Book> books)
+        {
+            Books = books;
+        }
+        public void Print()
+        {
+            foreach(Book b in Books)
+                Console.WriteLine($"{b.Name,12}{b.Autor,20}{b.Year,6}");
+        }
+
+        public Library()
+        {
+        }
+    }
+}
--- HW-OOP-2/Program.cs
+++ HW-OOP-2/Program.cs
@@ -1,15 +1,28 @@
 ﻿using HW_OOP_2;
 
-Console.WriteLine("Введите необходимое значение элементов массива");
-int n;
-int.TryParse(Console.ReadLine(), out n);
-DinamicArray array = new DinamicArray(n);
-Console.WriteLine("Заполненный случайными числами массив:");
-array.masRandom();
-array.masPrint();
-Console.WriteLine();
-Console.WriteLine("Перемешанный массив:");
-array.masRandomChange();
-array.masPrint();
-Console.WriteLine();
-array.masCount();
+//Console.WriteLine("Введите необходимое значение элементов массива");
+//int n;
+//int.TryParse(Console.ReadLine(), out n);
+//DinamicArray array = new DinamicArray(n);
+//Console.WriteLine("Заполненный случайными числами массив:");
+//array.masRandom();
+//array.masPrint();
+//Console.WriteLine();
+//Console.WriteLine("Перемешанный массив:");
+//array.masRandomChange();
+//array.masPrint();
+//Console.WriteLine();
+//array.masCount();
+
+Library library = new Library();
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("ведите название книги:");
+    string name = Console.ReadLine()!;
+    Console.Write("ведите автора:");
+    string autor = Console.ReadLine()!;
+    Console.Write("Введите год издания:");
+    int year = int.Parse(Console.ReadLine()!);
+    library.Add(new Book() { Name = name, Autor = autor, Year = year });
+}
+library.Print();
