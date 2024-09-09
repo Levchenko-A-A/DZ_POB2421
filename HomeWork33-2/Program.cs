e4bdf1f787881142e045a91616383159d2e6340e
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-09-09 15:32:45 +0200

    HomeWork33-2

--- HomeWork33-2/Program.cs
+++ HomeWork33-2/Program.cs
@@ -1,43 +1,74 @@
 ﻿
 
 using HomeWork33_2;
+using System.Diagnostics;
 Sort sort = new Sort();
-
-int[] mas = new int[100];
+Stopwatch stpWatch1 = new Stopwatch();
+Stopwatch stpWatch2 = new Stopwatch();
+Stopwatch stpWatch3 = new Stopwatch();
+Stopwatch stpWatch4 = new Stopwatch();
+Stopwatch stpWatch5 = new Stopwatch();
+int[] mas = new int[100000];
+int count = 100;
 Random rand = new Random();
-for (int i = 0; i < mas.Length; i++) mas[i] = rand.Next(1, 100);
-sort.Print(mas);
-Console.WriteLine("Пузырьковая сортировка:\n");
-sort.BubbleSort(mas);
-sort.Print(mas);
-sort.BubbleSortReverse(mas);
-sort.Print(mas);
-Console.WriteLine("Сортировка выбором:\n");
-for (int i = 0; i < mas.Length; i++) mas[i] = rand.Next(1, 100);
-sort.Print(mas);
-sort.SelectionSort(mas);
-sort.Print(mas);
-sort.SelectionSortReverse(mas);
-sort.Print(mas);
-Console.WriteLine("Сортировка вставкой:\n");
-for (int i = 0; i < mas.Length; i++) mas[i] = rand.Next(1, 100);
-sort.Print(mas);
-sort.InsertionSort(mas);
-sort.Print(mas);
-sort.InsertionSortReverse(mas);
-sort.Print(mas);
 
+for (int i = 0; i < mas.Length; i++) mas[i] = rand.Next(count);
+//sort.Print(mas);
+Console.WriteLine("Пузырьковая сортировка:\n");
+stpWatch1.Start();
+sort.BubbleSort(mas);
+stpWatch1.Stop();
+Console.WriteLine("Bubble Sort StopWatch: " + (stpWatch1.ElapsedMilliseconds / 1000.0).ToString());
+//sort.Print(mas);
+sort.BubbleSortReverse(mas);
+//sort.Print(mas);
+
+Console.WriteLine("Сортировка выбором:\n");
+for (int i = 0; i < mas.Length; i++) mas[i] = rand.Next(count);
+//sort.Print(mas);
+stpWatch2.Start();
+sort.SelectionSort(mas);
+stpWatch2.Stop();
+Console.WriteLine("Selection Sort StopWatch: " + (stpWatch2.ElapsedMilliseconds / 1000.0).ToString());
+//sort.Print(mas);
+sort.SelectionSortReverse(mas);
+//sort.Print(mas);
+
+Console.WriteLine("Сортировка вставкой:\n");
+for (int i = 0; i < mas.Length; i++) mas[i] = rand.Next(count);
+//sort.Print(mas);
+stpWatch3.Start();
+sort.InsertionSort(mas);
+stpWatch3.Stop();
+Console.WriteLine("Insertion Sort StopWatch: " + (stpWatch3.ElapsedMilliseconds / 1000.0).ToString());
+//sort.Print(mas);
+sort.InsertionSortReverse(mas);
+//sort.Print(mas);
+
+Console.WriteLine("Cортировка слиянием:\n");
+for (int i = 0; i < mas.Length; i++) mas[i] = rand.Next(count);
+//sort.Print(mas);
+stpWatch4.Start();
+sort.MergeSort(mas, 0, mas.Length - 1);
+stpWatch4.Stop();
+Console.WriteLine("Merge Sort StopWatch: " + (stpWatch4.ElapsedMilliseconds / 1000.0).ToString());
+//sort.Print(mas);
+sort.MergeSortRevers(mas, 0, mas.Length - 1);
+//sort.Print(mas);
 
 Console.WriteLine("Быстрая сортировка:\n");
-for (int i = 0; i < mas.Length; i++) mas[i] = rand.Next(1, 100);
-sort.Print(mas);
-sort.QuickSort(mas,0,mas.Length-1);
-sort.Print(mas);
-sort.QuickSortReverse(mas, 0, mas.Length-1);
-sort.Print(mas);
+for (int i = 0; i < mas.Length; i++) mas[i] = rand.Next(count);
+//sort.Print(mas);
+stpWatch5.Start();
+sort.QuickSort(mas, 0, mas.Length - 1);
+stpWatch5.Stop();
+Console.WriteLine("Quick Sort StopWatch: " + (stpWatch5.ElapsedMilliseconds / 1000.0).ToString());
+//sort.Print(mas);
+sort.QuickSortReverse(mas, 0, mas.Length - 1);
+//sort.Print(mas);
 
-//sort.SelectionSort(mas);
-//sort.InsertionSort(mas);
-//sort.MergeSort(mas, 0, mas.Length - 1);
-//sort.QuickSort(mas, 0, mas.Length - 1);
-
+Console.WriteLine("Группировка Четные и нечетные числа:\n");
+for (int i = 0; i < mas.Length; i++) mas[i] = rand.Next(count);
+//sort.Print(mas);
+mas=sort.EvenOddNumber(mas);
+//sort.Print(mas);
--- HomeWork33-2/Sort.cs
+++ HomeWork33-2/Sort.cs
@@ -102,19 +102,13 @@ namespace HomeWork33_2
             int n2 = right - middle;
             int[] leftArr = new int[n1];
             int[] rightArr = new int[n2];
-            for (int i = 0; i < n1; i++)
-            {
-                leftArr[i] = arr[left + 1];
-            }
-            for (int j = 0; j < n2; j++)
-            {
-                leftArr[j] = arr[middle + 1 + j];
-            }
+            for (int i = 0; i < n1; i++) leftArr[i] = arr[left + i];
+            for (int j = 0; j < n2; j++) rightArr[j] = arr[middle + 1 + j];
             int k = left;
             int p = 0, q = 0;
             while (p < n1 && q < n2)
             {
-                if (leftArr[p] == rightArr[q])
+                if (leftArr[p] <= rightArr[q])
                 {
                     arr[k] = leftArr[p];
                     p++;
@@ -147,6 +141,53 @@ namespace HomeWork33_2
                 MergeSort(arr, left, middle);
                 MergeSort(arr, middle + 1, right);
                 Merge(arr, left, middle, right);
+            }
+        }
+        private void MergeRevers(int[] arr, int left, int middle, int right) //сортировка слиянием
+        {
+            int n1 = middle - left + 1;
+            int n2 = right - middle;
+            int[] leftArr = new int[n1];
+            int[] rightArr = new int[n2];
+            for (int i = 0; i < n1; i++) leftArr[i] = arr[left + i];
+            for (int j = 0; j < n2; j++) rightArr[j] = arr[middle + 1 + j];
+            int k = left;
+            int p = 0, q = 0;
+            while (p < n1 && q < n2)
+            {
+                if (leftArr[p] >= rightArr[q])
+                {
+                    arr[k] = leftArr[p];
+                    p++;
+                }
+                else
+                {
+                    arr[k] = rightArr[q];
+                    q++;
+                }
+                k++;
+            }
+            while (p < n1)
+            {
+                arr[k] = leftArr[p];
+                p++;
+                k++;
+            }
+            while (q < n2)
+            {
+                arr[k] = rightArr[q];
+                q++;
+                k++;
+            }
+        }
+        public void MergeSortRevers(int[] arr, int left, int right) //сортировка слиянием
+        {
+            if (left < right)
+            {
+                int middle = (left + right) / 2;
+                MergeSortRevers(arr, left, middle);
+                MergeSortRevers(arr, middle + 1, right);
+                MergeRevers(arr, left, middle, right);
             }
         }
         public int Partition(int[] arr, int low, int high) //быстрая сортировка
@@ -208,6 +249,10 @@ namespace HomeWork33_2
                 QuickSortReverse(arr, pivotIndex + 1, high);
             }
 
+        }
+        public int[] EvenOddNumber(int[] arr)
+        {
+            return arr.Where(e => e % 2 == 0 ).Concat(arr.Where(e => e % 2 != 0)).ToArray();
         }
         public void Print(int[] arr)
         {
