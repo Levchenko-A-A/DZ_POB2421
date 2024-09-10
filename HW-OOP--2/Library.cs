e447458f378231b5313d8fc12eaa2733f2ee196f
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-09-10 11:57:15 +0200

    OOP--2

---
+++ HW-OOP--2/Library.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HW_OOP__2
+{
+    struct Book
+    {
+        private string? name;
+        public string? Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string? autor;
+        public string Autor
+        {
+            get { return autor; }
+            set { autor = value; }
+        }
+        private int year;
+        public int Year
+        {
+            get { return year; }
+            set { if (value > 0) year = value;
+                else throw new Exception("Введите корректно дату.");
+            }
+        }
+    }
+    internal class Library
+    {
+        public List<Book> Books { get; set; } = new();
+        //public Library()
+        //{
+        //}
+        public void Add(Book book) => Books.Add(book);
+        public void SubName(string? name)
+        {
+            if(FindIndexName(name)!=-1)
+            Books.RemoveAll(i => i.Name == name);
+            else Console.WriteLine("Книги с таким названием нет в базе:");
+        }
+        public void SubAutor(string? autor)
+        {
+            if (FindIndexAutor(autor) != -1)
+                Books.RemoveAll(i => i.Autor == autor);
+            else Console.WriteLine("Книги такого Автора нет в базе:");
+        }
+        public void SubYear(int year)
+        {
+            if (FindIndexYear(year) != -1)
+                Books.RemoveAll(i => i.Year == year);
+            else Console.WriteLine("Книги с таким годом выпуска нет в базе:");
+        }
+        public Library(List<Book> books)
+        {
+            Books = books;
+        }
+        public void Print()
+        {
+            foreach (Book b in Books)
+                Console.WriteLine($"{b.Name,12}{b.Autor,20}{b.Year,6}");
+        }
+        public void FindName(string name)
+        {
+            foreach (Book b in Books)
+            {
+                if (b.Name == name)
+                    Console.WriteLine($"{b.Name,12}{b.Autor,20}{b.Year,6}");
+            }
+            Console.WriteLine("Нажмите любую клавишу.");
+            Console.ReadKey();
+        }
+        public void FindAutor(string name)
+        {
+            foreach (Book b in Books)
+            {
+                if (b.Autor == name)
+                    Console.WriteLine($"{b.Name,12}{b.Autor,20}{b.Year,6}");
+            }
+            Console.WriteLine("Нажмите любую клавишу.");
+            Console.ReadKey();
+        }
+        public void FindYear(int year)
+        {
+            foreach (Book b in Books)
+            {
+                if (b.Year == year)
+                    Console.WriteLine($"{b.Name,12}{b.Autor,20}{b.Year,6}");
+            }
+            Console.WriteLine("Нажмите любую клавишу.");
+            Console.ReadKey();
+        }
+
+        public int FindIndexName(string name)
+        {
+            foreach (Book b in Books)
+            {
+                int index = 0;
+                if (b.Name == name)
+                {
+                    index++;
+                    return index;
+                }
+            }
+            return -1;
+        }
+        public int FindIndexAutor(string autor)
+        {
+            foreach (Book b in Books)
+            {
+                int index = 0;
+                if (b.Autor == autor)
+                {
+                    index++;
+                    return index;
+                }
+            }
+            return -1;
+        }
+        public int FindIndexYear(int year)
+        {
+            foreach (Book b in Books)
+            {
+                int index = 0;
+                if (b.Year == year)
+                {
+                    index++;
+                    return index;
+                }
+            }
+            return -1;
+        }
+
+        public void sortName()
+        {
+            for (int i = 1; i < Books.Count; i++)
+            {
+                Book key = Books[i];
+                int j = i - 1;
+                while (j >= 0 && Books[j].Name.CompareTo(key.Name) > 0)
+                {
+                    Books[j + 1] = Books[j];
+                    j--;
+                }
+                Books[j + 1] = key;
+            }
+        }
+        public void sortAutor()
+        {
+            for (int i = 1; i < Books.Count; i++)
+            {
+                Book key = Books[i];
+                int j = i - 1;
+                while (j >= 0 && Books[j].Autor.CompareTo(key.Autor) > 0)
+                {
+                    Books[j + 1] = Books[j];
+                    j--;
+                }
+                Books[j + 1] = key;
+            }
+        }
+        public Library()
+        {
+        }
+    }
+}
--- HW-OOP--2/Program.cs
+++ HW-OOP--2/Program.cs
@@ -1,8 +1,46 @@
 ﻿using HW_OOP__2;
 //Задание 1
-DinArray dinArray = new DinArray();
-dinArray.Print();
-dinArray.Rearrange();
-dinArray.Print();
-dinArray.DinCount();
+//DinArray dinArray = new DinArray();
+//dinArray.Print();
+//dinArray.Rearrange();
+//dinArray.Print();
+//dinArray.DinCount();
 //Задание 2
+Library library = new();
+for (int i = 0; i < 2; i++)
+{
+    Console.Write("ведите название книги:");
+    string name = Console.ReadLine()!;
+    Console.Write("ведите автора:");
+    string autor = Console.ReadLine()!;
+    Console.Write("Введите год издания:");
+    int year = int.Parse(Console.ReadLine()!);
+    library.Add(new Book() { Name = name, Autor = autor, Year = year });
+}
+Console.Clear();
+Console.WriteLine("Исходный список");
+library.Print();
+Console.WriteLine("Список отсортирован по Названию Книги.");
+library.sortName();
+library.Print();
+Console.Write("Для удаления книги по названию, ведите название книги:");
+string nameBook = Console.ReadLine()!;
+library.SubName(nameBook);
+library.Print();
+Console.Write("Для удаления книги по автору, ведите автора:");
+string autorBook = Console.ReadLine()!;
+library.SubAutor(autorBook);
+library.Print();
+//Console.Write("Для удаления книги по году издания, введите год издания:");
+//int yearBook = int.Parse(Console.ReadLine()!);
+//library.SubYear(yearBook);
+//library.Print();
+//Console.Write("Для вывода списка по названию, ведите название книги:");
+//nameBook = Console.ReadLine()!;
+//library.FindName(nameBook);
+//Console.Write("Для вывода списка по автору, ведите автора:");
+//autorBook = Console.ReadLine()!;
+//library.FindAutor(autorBook);
+//Console.Write("Для вывода списка по году издания, введите год издания:");
+//yearBook = int.Parse(Console.ReadLine()!);
+//library.FindYear(yearBook);
