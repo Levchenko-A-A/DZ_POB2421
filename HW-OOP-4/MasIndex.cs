3561c7d2c7b38b76dd9f5deb2da49c74bda0481d
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-09-12 16:40:06 +0200

    OOP-4

---
+++ HW-OOP-4/MasIndex.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_OOP_4
+{
+    internal class MasIndex
+    {
+        private string[]? masString;
+        public int lengMas {  get; set; }
+        public int count { get; } = 20; 
+        private bool errFlag;
+        public MasIndex(int lenght)
+        {
+            this.lengMas = lenght;
+            errFlag=false;
+            masString = new string[lenght];
+        }
+        public string this[int index]
+        {
+            get 
+            { 
+                if(Ok(index))
+                {
+                    errFlag = false;
+                    return masString[index];
+                }
+                else
+                {
+                    Console.WriteLine("Индекс введен не верно.");
+                    errFlag = true;
+                    return null;
+                }
+            }
+            set
+            {
+                if (Ok(index))
+                {
+                    masString[index] = value;
+                    errFlag = false;
+                }
+                errFlag = true;
+            }
+        }
+        private bool Ok(int index)
+        {
+            if (index >= 0 && index < lengMas)
+                return true;
+            return false;
+        }
+        public void Fill()
+        {
+            Random random = new Random();
+            for (int i = 0; i < lengMas; i++)
+            {
+                for (int j = 0; j < count; j++)
+                {
+                    masString[i] += random.Next(0, 10).ToString();
+                    
+                }
+            }
+        }
+        public MasIndex Merge(MasIndex first, MasIndex second)
+        {
+            int tmpLenght=first.lengMas+second.lengMas;
+            MasIndex tmp = new MasIndex(tmpLenght);
+            if (first.lengMas == second.lengMas)
+            {
+                for (int i = 0; i < first.lengMas; i++)
+                {
+                    tmp[i] = first[i];
+                    tmp[i+lengMas] = second[i];
+                }
+            }
+            return tmp;
+        }
+        public MasIndex MasDistinc (MasIndex temp)
+        {
+            
+            for(int i=0; i<temp.lengMas;i++)
+            {
+                string tmp = temp[i];
+                char[] tmp1 = tmp.Distinct().ToArray();
+                tmp = new string(tmp1);
+                temp[i] = tmp;
+            }
+            return temp;
+        }
+    }
+}
---
+++ HW-OOP-4/Program.cs
@@ -0,0 +1,35 @@
+﻿using HW_OOP_4;
+
+MasIndex masIndexOne = new MasIndex(5);
+MasIndex masIndexTwo = new MasIndex(5);
+MasIndex masIndexThird = new MasIndex(masIndexOne.lengMas+masIndexTwo.lengMas);
+masIndexOne.Fill();
+masIndexTwo.Fill();
+masIndexThird.Fill();
+Console.WriteLine("Вывод на экран первого массива:");
+for (int i = 0; i < masIndexOne.lengMas; i++)
+    Console.WriteLine(masIndexOne[i]);
+Console.WriteLine("Вывод на экран второго массива:");
+for (int i = 0; i < masIndexTwo.lengMas; i++)
+    Console.WriteLine(masIndexTwo[i]);
+masIndexThird =masIndexOne.Merge(masIndexOne, masIndexTwo);
+Console.WriteLine("Вывод на экран объединенного третьево массива:");
+for (int i = 0; i < masIndexThird.lengMas; i++)
+    Console.WriteLine(masIndexThird[i]);
+Console.WriteLine();
+masIndexThird = masIndexOne.MasDistinc(masIndexThird);
+Console.WriteLine("Вывод на экран третьево массива без дубликатов:");
+for (int i = 0; i < masIndexThird.lengMas; i++)
+    Console.WriteLine(masIndexThird[i]);
+Console.WriteLine("Нажмите любую клавишу.");
+Console.ReadKey();
+Console.Clear();
+Console.WriteLine("Вывод на экран первого массива:");
+for (int i = 0; i < masIndexOne.lengMas; i++)
+    Console.WriteLine(masIndexOne[i]);
+Console.WriteLine("Какую строку вывести на экран отдельно?:");
+int n;
+int.TryParse(Console.ReadLine()!, out n);
+Console.WriteLine(masIndexOne[n - 1]);
+Console.ReadLine();
+Console.WriteLine("Нажмите любую клавишу.");
