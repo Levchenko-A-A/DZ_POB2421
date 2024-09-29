983ae71d8e77f2842cdf55d40e5475b43818c6fd
Андрей Левченков <lev_a_a@mail.ru>
2024-09-29 18:13:11 +0200

    HW-OOP-12

---
+++ HW-OOP-12/Point3D.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_OOP_12
+{
+    internal class Point3D
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        public Point3D(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public Point3D()
+        {
+            X = 0;
+            Y = 0;
+            Z = 0;
+        }
+        public static double LengsVector(Point3D vector)
+        {
+            return Math.Round(Math.Sqrt(Math.Pow(vector.X, 2) + Math.Pow(vector.Y, 2) + Math.Pow(vector.Z, 2)),2);
+        }
+        public static double ABC(Point3D vector)
+        {
+            return Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y + vector.Z * vector.Z);
+        }
+        public static Point3D operator + (Point3D left, Point3D right)
+        {
+            return new Point3D(left.X + right.X, left.Y + right.Y, left.Z + right.Z);
+        }
+        public static Point3D operator - (Point3D left, Point3D right)
+        {
+            return new Point3D(left.X - right.X, left.Y - right.Y, left.Z - right.Z);
+        }
+        public static Point3D operator - (Point3D left)
+        {
+            return new Point3D(-left.X,  -left.Y, -left.Z);
+        }
+        public static double operator * (Point3D left, Point3D right)
+        {
+            return left.X * right.X + left.Y * right.Y + left.Z * right.Z;
+        }
+        public override string? ToString()
+        {
+            return $"Координаты вектора: {X}, {Y}, {Z}";
+        }
+    }
+}
---
+++ HW-OOP-12/Program.cs
@@ -0,0 +1,12 @@
+﻿using HW_OOP_12;
+using System.Numerics;
+using System.Security.Cryptography;
+
+Point3D point2 = new Point3D(7, 3, 15);
+Point3D point3 = new Point3D(-5, 10, 3);
+Console.WriteLine($"Длина вектора:{Point3D.LengsVector(point2)}");
+Console.WriteLine($"Модуль вектора:{Math.Round(Point3D.ABC(point3),2)}");
+Point3D pointSum = point2 + point3;
+Console.WriteLine($"Сумма векторов:\n{pointSum}");
+Console.WriteLine($"Скалярное произведене векторов: {Math.Round(point2*point3,2)}");
+Console.WriteLine($"Угол между векторами: {Math.Round(point2*point3/(Point3D.ABC(point2)*Point3D.ABC(point3)),2)}");
