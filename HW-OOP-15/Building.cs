78a0d6abe256689b46d14926da0d5e5bc6f0a4ce
Андрей Левченков <lev_a_a@mail.ru>
2024-10-01 21:45:18 +0200

    HW-OOP-15

---
+++ HW-OOP-15/Building.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_OOP_15
+{
+    internal class Building
+    {
+        public delegate double Сalc(Building bg);
+        public static Сalc operation = CalculateVolume;
+
+        public string? BuildingName { get; set; }
+        public double Height { get; set; }
+        public int Floors { get; set; }
+        public double TotalArea { get; set; }
+        public string? Location { get; set; }
+
+        public Building(string? buildingName, double height, int floors, double totalArea, string? location)
+        {
+            BuildingName = buildingName;
+            Height = height;
+            Floors = floors;
+            TotalArea = totalArea;
+            Location = location;
+        }
+        public static double CalculateVolume(Building bg)
+        {
+            return bg.Height*bg.TotalArea;
+        }
+
+        public static void Volume(List<Building> bd)
+        {
+            foreach (Building i in bd)
+            {
+                Console.WriteLine($"Название: {i.BuildingName}");
+                Console.WriteLine($"Высота: {i.Height} м");
+                Console.WriteLine($"Количество этажей: {i.Floors}");
+                Console.WriteLine($"Общая площадь: {i.TotalArea} кв.м.");
+                Console.WriteLine($"Местоположение: {i.Location}");
+                Console.WriteLine($"Объём: {operation(i)} куб.м.");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ HW-OOP-15/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using HW_OOP_15;
+List<Building> buildings = new List<Building>();
+Building building = new Building("Жилой дом 1", 115, 38, 500, "Мегаполис");
+buildings.Add(building);
+building = new Building("Жилой дом 2", 30, 10, 250, "Поселок");
+buildings.Add(building);
+building = new Building("Жилой дом 3", 70, 17, 450, "Город");
+buildings.Add(building);
+Building.Volume(buildings);
+
+
+
+
