ad452839908b1a42d7383181f6b545f9792c7ab2
Андрей Левченков <lev_a_a@mail.ru>
2024-10-05 19:45:13 +0200

    HW-OOP-13

---
+++ HW-OOP-13/Omnivorous.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_OOP_13
+{
+    public class Omnivorous : Animal
+    {
+        double weight;
+        public override double Weight
+        {
+            get => weight;
+            set { if (value > 0) weight = value; }
+        }
+        public override string? TypeOfFood => "Травоядное";
+        double normOnePer;
+        public override double NormOnePer
+        {
+            get => normOnePer;
+            set { if (value > 0) normOnePer = value; }
+        }
+        public override double FoodQuantity
+        {
+            get { return Math.Round(AmountOfFood(), 3); }
+        }
+        public Omnivorous(int id, string? name, double weight, int age, double normOnePer) : base(id, name, age)
+        {
+            Weight = weight;
+            NormOnePer = normOnePer;
+        }
+        public override double AmountOfFood()
+        {
+            return weight * normOnePer;
+        }
+
+        public override string? ToString()
+        {
+            return $"{Id,4} {Name,-8}{Age,-9}{Weight,-7}{NormOnePer,-11}{TypeOfFood,-25}{AmountOfFood()}";
+        }
+    }
+}
---
+++ HW-OOP-13/Predator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_OOP_13
+{
+    internal class Predator : Animal
+    {
+        double weight;
+        public override double Weight
+        {
+            get => weight;
+            set { if (value > 0) weight = value; }
+        }
+        public override string? TypeOfFood => "Хищное";
+        double normOnePer;
+        public override double NormOnePer
+        {
+            get => normOnePer;
+            set { if (value > 0) normOnePer = value; }
+        }
+        public override double FoodQuantity
+        {
+            get { return AmountOfFood(); }
+        }
+        public Predator(int id, string? name, double weight, int age, double normOnePer) : base(id, name, age)
+        {
+            Weight = weight;
+            NormOnePer = normOnePer;
+        }
+        public override double AmountOfFood()
+        {
+            return Math.Round(weight * normOnePer, 3);
+        }
+
+        public override string? ToString()
+        {
+            return $"{Id,4} {Name,-8}{Age,-9}{Weight,-7}{NormOnePer,-11}{TypeOfFood,-25}{AmountOfFood()}";
+        }
+    }
+}
