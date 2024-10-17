43e553a9d45412a7951a562e9e829e133d82e821
Андрей Левченков <lev_a_a@mail.ru>
2024-10-17 19:00:51 +0200

    19.1

--- HW-OOP-19.1/Program.cs
+++ HW-OOP-19.1/Program.cs
@@ -12,9 +12,30 @@ taskManager.Add(new Event()
     Prioity = Prioity.High,
     Title = "t2"
 });
+taskManager.Add(new Event()
+{
+    DueDate = DateTime.Parse("15.11.2024"),
+    Location = "Anapa",
+    Prioity = Prioity.Low,
+    Title = "t3"
+});
+taskManager.Add(new Task()
+{
+    DueDate = DateTime.Parse("26.05.2025"),
+    Prioity = Prioity.High,
+    Title = "t4"
+});
+
 taskManager.Print();
 taskManager.Remove(taskManager.Tasks[1]);
+Console.WriteLine();
 taskManager.Print();
+
+Console.WriteLine("Какую запись необходимо изменить?:");
+int n = int.Parse(Console.ReadLine()!);
+taskManager.ChangeTaskOrEvent(taskManager.Tasks[n-1]);
+taskManager.Print();
+
 enum Prioity
 {
     Low,
@@ -24,7 +45,7 @@ enum Prioity
 
 class Task : ITask
 {
-    public string Title { get; set; }
+    public string? Title { get; set; }
     public DateTime DueDate { get; set; }
     public Prioity Prioity { get; set; }
 
@@ -60,8 +81,50 @@ class TaskManager
     }
     public void Print()
     {
+        int j = 1;
         foreach (var item in Tasks)
+        {
+            Console.Write(j++ + " ");
             item.Display();
+        }
+    }
+    public void ChangeTaskOrEvent(ITask task)
+    {
+        if (task is Task)
+        {
+            task.Display();
+            Console.Write("Введите новое название задачи:");
+            string str = Console.ReadLine()!;
+            task.Title = str;
+            Console.Write("Введите новую дату:");
+            DateTime temtDate = DateTime.Parse(Console.ReadLine()!);
+            task.DueDate = temtDate;
+            Console.Write("Введите новsq приоритет задачи (Low, Medium, High):");
+            str = Console.ReadLine()!;
+            if (Enum.IsDefined(typeof(Prioity), str))
+                task.Prioity = (Prioity)Enum.Parse(typeof(Prioity), str, true);
+            else
+                Console.WriteLine("Значение приоритета введено не верно.");
+        }
+        else if (task is Event)
+        {
+            task.Display();
+            Console.Write("Введите новое название задачи:");
+            string str = Console.ReadLine()!;
+            task.Title = str;
+            Console.Write("Введите новую дату:");
+            DateTime temtDate = DateTime.Parse(Console.ReadLine()!);
+            task.DueDate = temtDate;
+            Console.Write("Введите новsq приоритет задачи (Low, Medium, High):");
+            str = Console.ReadLine()!;
+            if (Enum.IsDefined(typeof(Prioity), str))
+                task.Prioity = (Prioity)Enum.Parse(typeof(Prioity), str, true);
+            else
+                Console.WriteLine("Значение приоритета введено не верно.");
+            Console.Write("Введите новое локацию задачи:");
+            str = Console.ReadLine()!;
+            (Event)task.Location = str;
+        }
     }
 
 }
