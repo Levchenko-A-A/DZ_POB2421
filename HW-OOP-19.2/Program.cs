0e2d1a4baf4502c704332d40944ec7380cbad2c2
Андрей Левченков <lev_a_a@mail.ru>
2024-10-20 09:20:59 +0200

    19.2

---
+++ HW-OOP-19.2/Program.cs
@@ -0,0 +1,39 @@
+﻿enum Prioity
+{
+    Low,
+    Medium,
+    High
+};
+class Employee
+{
+    public string? Name { get; set; }
+    private int id;
+    public int Id 
+    {  
+        get { return id; } 
+        set { if (value > 0) id = value; } 
+    }   
+     
+}
+
+class Task: ICloneable
+{
+    public string? Title { get; set; }
+    public DateTime DueDate { get; set; }
+    public Prioity Prioity { get; set; }
+
+    public object Clone()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Display()
+    {
+        Console.WriteLine(Title + " " + DueDate + " " + Prioity);
+    }
+
+    public override string? ToString()
+    {
+        return $"{"Task"},{Title},{DueDate},{Prioity}";
+    }
+}
