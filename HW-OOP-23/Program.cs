5a0db9e40f34d87c83ddadb92a0a3ed685ce349a
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-10-24 15:34:56 +0200

    23

---
+++ HW-OOP-23/Program.cs
@@ -0,0 +1,23 @@
+﻿Keyboard simbol = new Keyboard();
+simbol.KeyPressed += DisplayChar;
+do
+{
+    char ch = Console.ReadKey(true).KeyChar;
+    if ((int)ch == 27) break;
+    simbol.Put(ch);
+}
+while (true);
+
+void DisplayChar(string message) => Console.Write(message);
+
+class Keyboard
+{
+    public delegate void KeyEventArgs(string message);
+    public event KeyEventArgs? KeyPressed;
+    public int Key { get; private set; }
+    //public Keyboard(int key) => Key = key;
+    public void Put(char key)
+    {
+        KeyPressed?.Invoke($"Вы нажали Клавишу: {key}\n");
+    }
+}
