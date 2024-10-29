980ac4421684ddfb740d85cd3ed936b08776b1f0
Андрей Левченков <lev_a_a@mail.ru>
2024-10-29 19:58:55 +0200

    22

---
+++ HW-OOP-22/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+
+Console.Write("Введите UserName:");
+string? userName = Console.ReadLine()!;
+Console.Write("Введите UserLogin:");
+string? userLogin = Console.ReadLine()!;
+Console.Write("Введите Password:");
+string? password = Console.ReadLine()!;
+User newUser = new(userName, userLogin, password);
+public class User: IUser
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Login { get; set; }
+    public string? Password { get; set; }
+    public User(string? name, string? login, string? password)
+    {
+        string[] userData;
+        if (File.Exists("User.txt"))
+            {
+            userData = File.ReadAllLines("User.txt");
+            for (int i = 0; i < userData.Length; i++)
+            {
+                if (userData[i].Split(" ")[2] == login)
+                {
+                    Console.WriteLine("Пользователь с таким логином существует");
+                    return;
+                }
+            }
+        }
+        else userData = new string[0];
+        Id = userData.Length;
+        try
+        {
+            Name = new IsDataExists((data) => { if (string.IsNullOrEmpty(data)) { throw new Exception("Нет данных"); } return data; })(name!);
+            Login = new IsDataExists((data) => { if (string.IsNullOrEmpty(data)) { throw new Exception("Нет данных"); } return data; })(login!);
+            Password = new IsDataExists((data) => { if (string.IsNullOrEmpty(data)) { throw new Exception("Нет данных"); } return data; })(password!);
+        }
+        catch (Exception e) 
+        {
+            Console.WriteLine(e.Message);
+        }
+        SaveData($"{Id} {Name} {Login} {Password}");
+    }
+    private void SaveData(string dataLine)
+    {
+        string[] tempData;
+        if (File.Exists("User.txt"))
+            tempData = File.ReadAllLines("User.txt");
+        else tempData = new string[0];
+        List<string> data = new List<string>();
+        data.AddRange(tempData);
+        data.Add(dataLine);
+        File.WriteAllLines("User.txt", data);
+    }
+}
+internal interface IUser
+{
+    public int Id { set; }
+    public string? Name { set; }
+    public string? Login { set; }
+    public string? Password { set; }
+}
+public delegate string IsDataExists(string data);
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,21 +1,46 @@
 ﻿
-string str = Console.ReadLine();
-Console.WriteLine(str);
-Console.WriteLine(PerevodX(str));
+//string str = Console.ReadLine();
+//Console.WriteLine(str);
+//Console.WriteLine(PerevodX(str));
 
 
-int PerevodX(string temp)
+//int PerevodX(string temp)
+//{
+//    int result = 0;
+//    if (temp == "A") result = 0;
+//    else if (temp == "Б") result = 1;
+//    else if (temp == "В") result = 2;
+//    else if (temp == "Г") result = 3;
+//    else if (temp == "Д") result = 4;
+//    else if (temp == "Е") result = 5;
+//    else if (temp == "Ж") result = 6;
+//    else if (temp == "З") result = 7;
+//    else if (temp == "И") result = 8;
+//    else if (temp == "К") result = 9;
+//    return result;
+//}
+using System.Xml.Linq;
+IsDataExists Chek = delegate (string data)
 {
-    int result = 0;
-    if (temp == "A") result = 0;
-    else if (temp == "Б") result = 1;
-    else if (temp == "В") result = 2;
-    else if (temp == "Г") result = 3;
-    else if (temp == "Д") result = 4;
-    else if (temp == "Е") result = 5;
-    else if (temp == "Ж") result = 6;
-    else if (temp == "З") result = 7;
-    else if (temp == "И") result = 8;
-    else if (temp == "К") result = 9;
-    return result;
-}
+    try
+    {
+        if (string.IsNullOrEmpty(data))
+        {
+            throw new Exception("Нет данных");
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    return data;
+};
+Console.Write("Введите UserName:");
+string? userName = Console.ReadLine()!;
+Console.WriteLine(Chek(userName));
+
+
+
+
+
+delegate string IsDataExists(string data);
