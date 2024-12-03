b834eef25dd33beff9fc39ed82936c12ec27c915
Андрей Левченков <lev_a_a@mail.ru>
2024-12-03 20:31:35 +0200

    HW-OOP-28.1

---
+++ HW-OOP-28.1/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_OOP_28._1
+{
+    internal class Client
+    {
+        public string? Name { get; set; }
+        public string? SurName { get; set; }
+        public string? Email { get; set; }
+
+        public Client(string? name, string? surName, string? email)
+        {
+            Name = name;
+            SurName = surName;
+            Email = email;
+        }
+    }
+}
--- HW-OOP-28.1/Program.cs
+++ HW-OOP-28.1/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using HW_OOP_28._1;
+using OfficeOpenXml;
+using System.Diagnostics.Metrics;
+using System.Drawing.Drawing2D;
+
+string filePath = "СписокКлиентов.xlsx";
+ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+ExcelPackage newBook = new ExcelPackage(filePath);
+//newBook.Workbook.Worksheets.Add("Клиенты");
+ExcelWorksheet currentWork = newBook.Workbook.Worksheets["Клиенты"];
+List<Client> clients = new List<Client>();
+clients.Add(new Client("Иван", "Иванов", "ivan@example.com"));
+clients.Add(new Client("Петр", "Петров", "peter@example.com"));
+clients.Add(new Client("Мария", "Сидорова", "maria@example.com"));
+clients.Add(new Client("Анна", "Кузнецова", "anna@example.com"));
+clients.Add(new Client("Алексей", "Смирнов", "alex@example.com"));
+clients.Add(new Client("Наталья", "Васильева", "natalia@example.com"));
+clients.Add(new Client("Сергей", "Морозов", "sergey@example.com"));
+clients.Add(new Client("Ольга", "Павлова", "olga@example.com"));
+clients.Add(new Client("Денис", "Жуков", "denis@example.com"));
+clients.Add(new Client("Екатерина", "Макарова", "ekaterina@example.com"));
+currentWork.Cells["A1"].Value = "Имя";
+currentWork.Cells["B1"].Value = "Фамилия";
+currentWork.Cells["C1"].Value = "Почта";
+
+int currentRow = 2;
+foreach (Client client in clients)
+{
+    currentWork.Cells[currentRow, 1].Value = client.Name;
+    currentWork.Cells[currentRow, 2].Value = client.SurName;
+    currentWork.Cells[currentRow, 3].Value = client.Email;
+    currentRow++;
+}
+newBook.SaveAs("СписокКлиентов.xlsx");
