a46acc64f6926286a4f5c8ea30c4e1bed8dfc094
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-12-09 16:32:34 +0200

    HW-OOP-29.1

---
+++ HW-OOP-29.1/Person.cs
@@ -0,0 +1,17 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_OOP_29._1
+{
+    internal class Person
+    {
+        [Name("UserName")]
+        public string? Name { get; set; }
+        [Name("UserAge")]
+        public int Age { get; set; }  
+    }
+}
---
+++ HW-OOP-29.1/Program.cs
@@ -0,0 +1,42 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using HW_OOP_29._1;
+using System;
+using System.Globalization;
+
+List<Person> list = new List<Person>()
+{
+new Person { Name = "John" , Age = 30 },
+new Person { Name = "Alice" , Age = 25 }
+};
+CsvConfiguration csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+{
+    Delimiter = ";"
+};
+using (StreamWriter writer = new StreamWriter("output.csv"))
+{
+    
+    CsvWriter csvWrite = new CsvWriter(writer, csvConfig); 
+    list.Add(new Person() { Name = "Ivan", Age = 23 });
+    list.Add(new Person() { Name = "Sergei", Age = 19 });
+    list.Add(new Person() { Name = "Mike", Age = 23 });
+    list.Add(new Person() { Name = "Ivan", Age = 23 });
+    csvWrite.WriteRecords(list);
+    writer.Close();
+}
+List<Person> records;
+using (StreamReader reader = new StreamReader("output.csv"))
+{
+    CsvReader csvRead = new CsvReader(reader, csvConfig);
+    records = csvRead.GetRecords<Person>().ToList();
+    reader.Close();
+}
+foreach(Person tmp in list)
+{
+    Console.WriteLine($"Имя: {tmp.Name}\nВозраст: {tmp.Age}");
+}
+Console.WriteLine();
+foreach (Person person in records)
+{
+    Console.WriteLine($"Имя: {person.Name}\nВозраст: {person.Age}");
+}
