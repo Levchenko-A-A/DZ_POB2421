435c291c64fc430b5d6d5d14f3e3be9edde8b8e4
Levchenko-A-A <163921727+Levchenko-A-A@users.noreply.github.com>
2024-12-09 11:57:48 +0200

    HW-OOP-26.6

---
+++ HW-OOP26.6/Program.cs
@@ -0,0 +1,31 @@
+﻿GetFactAsync(5);
+for (int i = 1; i <= 10; i++)
+{
+    Thread.Sleep(i * 1000);
+    Console.WriteLine($"Секунда: {i}");
+}
+
+
+static async void GetFactAsync(int number)
+{
+    int result = 0;
+    Console.WriteLine("Метод поиска факториала стартовал");
+    await Task.Run(() => 
+    { 
+        result = CalculateFactorial(number);
+        Console.WriteLine($"Факториал {number}= {result}");
+    });
+    Thread.Sleep(10000);
+}
+static int CalculateFactorial(int number)
+{
+
+    int fact = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        Task.Delay(250).Wait();
+        fact *= i;
+    }
+    return fact;
+
+}
